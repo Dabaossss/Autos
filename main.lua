9cc5e7858b091d667e603df09ffc661c7e310161
Dabaossss <74158914+Dabaossss@users.noreply.github.com>
2020-11-09 08:00:19 +0800

    2

--- main.lua
+++ main.lua
@@ -1,4 +1,11 @@
 while true do
-mSleep(1000)
-print("大宝")
+mSleep(50)
+x, y = findColorInRegionFuzzy(0x191919, 95, 188, 210, 669, 373, 0, 0)
+if x > -1 then
+	touchDown(1,x,y) 
+	mSleep(50)
+	touchMove(1,x,y)
+	mSleep(50)	
+	touchUp(1,x,y)  
+end
 end
