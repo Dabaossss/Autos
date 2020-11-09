7a25cd5aefbb5f1276585f07ef5e865f0ae34d33
Dabaossss <74158914+Dabaossss@users.noreply.github.com>
2020-11-09 13:01:04 +0800

    更新 main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ while true do
 	print("扫描")
 	x, y = findColor({170, 145, 788, 433},
 		"0|0|0x171717,15|-3|0x181818,22|-14|0xf2f2f2,41|-15|0x171717,51|3|0xf2f2f2,58|-3|0x1b1b1b,82|-4|0x1b1b1b,95|-3|0xf3f3f3",
-		95, 0, 0, 0)
+		90, 0, 0, 0)
 	if x > -1 then
 		print("点击")
 		touchDown(1,x,y)
@@ -13,7 +13,7 @@ while true do
 	end
 	x, y = findColor({20, 81, 106, 175},
 		"0|0|0x191919,8|-2|0xe0e0e0,12|-8|0xdcdcdc,17|-16|0x191919,18|19|0x131313",
-		95, 0, 0, 0)
+		90, 0, 0, 0)
 	if x > -1 then
 		print("返回")
 		touchDown(1,x,y)
