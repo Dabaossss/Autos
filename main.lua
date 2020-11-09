e762ee9409f60faadaad7fa1b02b59853779c037
Dabaossss <74158914+Dabaossss@users.noreply.github.com>
2020-11-09 08:04:33 +0800

    更新 main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 while true do
 mSleep(50)
+print("扫描")
 x, y = findColor({188, 210, 669, 373}, 
 "0|0|0x191919,30|-8|0xf2f2f2,43|-4|0x131313,86|-5|0xf3f3f3,93|8|0x131313",
 95, 0, 0, 0)
 if x > -1 then
+	print("点击")
 	touchDown(1,x,y) 
 	mSleep(50)
 	touchMove(1,x,y)
