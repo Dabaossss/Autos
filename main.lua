0a95c7eb2e925ae059d4a8237e3163ad7c4dd58d
Dabaossss <74158914+Dabaossss@users.noreply.github.com>
2020-11-09 08:02:24 +0800

    3

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 while true do
 mSleep(50)
-x, y = findColorInRegionFuzzy(0x191919, 95, 188, 210, 669, 373, 0, 0)
+x, y = findColor({188, 210, 669, 373}, 
+"0|0|0x191919,30|-8|0xf2f2f2,43|-4|0x131313,86|-5|0xf3f3f3,93|8|0x131313",
+95, 0, 0, 0)
 if x > -1 then
 	touchDown(1,x,y) 
 	mSleep(50)
